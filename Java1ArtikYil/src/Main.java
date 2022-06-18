01255e6a573d669716eb76f32a94508373d68e23
İlhan Bayramoğlu <bayramogluilhan09@gmail.com>
2022-06-18 15:16:17 +0300

    Java101_1-ArtıkYılHesaplama

---
+++ Java1ArtikYil/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int yil;
+        System.out.print("Yıl Giriniz : ");
+        yil = input.nextInt();
+
+        if( yil%400==0 ){
+            System.out.println(yil + " artık yıldır.");
+        }
+        else if ( yil%400!=0 && yil%4==0 ) {
+            System.out.println(yil + " artık yıldır.");
+        }
+        else {
+            System.out.println(yil + " artık yıl değildir.");
+        }
+
+    }
+}
